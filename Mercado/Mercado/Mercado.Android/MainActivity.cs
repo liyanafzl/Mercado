3e15308930a2ebdfdc11a708810bad443167562b
Liyana Fizal <liyana.fzl@gmail.com>
2017-12-31 15:19:14 +0530

    Splash screen

--- Mercado/Mercado/Mercado.Android/MainActivity.cs
+++ Mercado/Mercado/Mercado.Android/MainActivity.cs
@@ -9,15 +9,16 @@ using Android.OS;
 
 namespace Mercado.Droid
 {
-    [Activity(Label = "Mercado", Icon = "@drawable/icon", Theme = "@style/MainTheme", MainLauncher = false, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    [Activity(Label = "Mercado", Icon = "@drawable/icon", Theme = "@style/Theme.splash", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle bundle)
         {
             TabLayoutResource = Resource.Layout.Tabbar;
             ToolbarResource = Resource.Layout.Toolbar;
-
+            base.SetTheme(Resource.Style.MainTheme);
             base.OnCreate(bundle);
+            
 
             global::Xamarin.Forms.Forms.Init(this, bundle);
             LoadApplication(new App());
